0a410d8d75510657a2afd5ef80918a855211c001
Otavio <estudosprogotavio@gmail.com>
2026-02-07 12:33:05 -0300

    feat:adicionando entidade jpa Usuario

---
+++ src/main/java/dev/zerphyis/orangezup/infra/persistence/entity/UsuarioEntity.java
@@ -0,0 +1,63 @@
+package dev.zerphyis.orangezup.infra.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuarios")
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+
+    protected UsuarioEntity() {
+    }
+
+    public UsuarioEntity(
+            Long id,
+            String nome,
+            String email,
+            String cpf,
+            LocalDate dataNascimento
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+}
