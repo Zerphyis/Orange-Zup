32347825df957d54a2aea642f57a1acac23524df
Otavio <estudosprogotavio@gmail.com>
2026-02-07 12:07:09 -0300

    feat:commit initial

---
+++ src/main/java/dev/zerphyis/orangezup/OrangezupApplication.java
@@ -0,0 +1,13 @@
+package dev.zerphyis.orangezup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangezupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangezupApplication.class, args);
+	}
+
+}
