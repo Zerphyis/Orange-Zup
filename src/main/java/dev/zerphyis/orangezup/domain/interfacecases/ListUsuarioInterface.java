8d556764cd55dfd203e85e6617351e83eaf1a7d8
Otavio <estudosprogotavio@gmail.com>
2026-02-09 04:43:22 -0300

    feat: adicionando interface ListUsuarioInterface

---
+++ src/main/java/dev/zerphyis/orangezup/domain/interfacecases/ListUsuarioInterface.java
@@ -0,0 +1,9 @@
+package dev.zerphyis.orangezup.domain.interfacecases;
+
+import dev.zerphyis.orangezup.domain.entities.Usuario;
+
+import java.util.List;
+
+public interface ListUsuarioInterface {
+    List<Usuario> execute();
+}
