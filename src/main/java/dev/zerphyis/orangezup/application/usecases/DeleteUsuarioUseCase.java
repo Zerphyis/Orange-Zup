c8c675d5773db13d21ac3cb3309d719c67da74fe
Otavio <estudosprogotavio@gmail.com>
2026-02-09 04:44:26 -0300

    feat: adicionando useCase DeleteUsuarioUseCase

---
+++ src/main/java/dev/zerphyis/orangezup/application/usecases/DeleteUsuarioUseCase.java
@@ -0,0 +1,24 @@
+package dev.zerphyis.orangezup.application.usecases;
+
+import dev.zerphyis.orangezup.domain.entities.repository.UsuarioRepository;
+import dev.zerphyis.orangezup.domain.exception.InvalidCpfException;
+import dev.zerphyis.orangezup.domain.interfacecases.DeleteUsuarioInterface;
+
+public class DeleteUsuarioUseCase implements DeleteUsuarioInterface {
+
+    private final UsuarioRepository repository;
+
+    public DeleteUsuarioUseCase(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void execute(String cpf) {
+        if (!repository.existsByCpf(cpf)) {
+            throw new InvalidCpfException("Usuário não encontrado");
+        }
+
+        repository.deleteByCpf(cpf);
+    }
+
+}
