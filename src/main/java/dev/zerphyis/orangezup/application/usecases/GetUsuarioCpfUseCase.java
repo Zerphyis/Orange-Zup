4faac90eb3a79fccae9ac6a7a2e3afcdf2bdc8d9
Otavio <estudosprogotavio@gmail.com>
2026-02-09 04:44:34 -0300

    feat: adicionando useCase GetUsuarioCpfUseCase

---
+++ src/main/java/dev/zerphyis/orangezup/application/usecases/GetUsuarioCpfUseCase.java
@@ -0,0 +1,21 @@
+package dev.zerphyis.orangezup.application.usecases;
+
+import dev.zerphyis.orangezup.domain.entities.Usuario;
+import dev.zerphyis.orangezup.domain.entities.repository.UsuarioRepository;
+import dev.zerphyis.orangezup.domain.exception.InvalidCpfException;
+import dev.zerphyis.orangezup.domain.interfacecases.GetCpfUsuarioInterface;
+
+public class GetUsuarioCpfUseCase implements GetCpfUsuarioInterface {
+    private final UsuarioRepository repository;
+
+    public GetUsuarioCpfUseCase(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public Usuario execute(String cpf) {
+        return repository.findByCpf(cpf)
+                .orElseThrow(() -> new InvalidCpfException("Usuário não encontrado"));
+    }
+}
