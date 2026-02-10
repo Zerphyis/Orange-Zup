7a1bcec3da18c9c08d763b66b76aeb559ae06c59
Otavio <estudosprogotavio@gmail.com>
2026-02-10 07:12:19 -0300

    feat:adicioando exeception InvalidVacinaException

---
+++ src/main/java/dev/zerphyis/orangezup/domain/exception/InvalidVacinaException.java
@@ -0,0 +1,7 @@
+package dev.zerphyis.orangezup.domain.exception;
+
+public class InvalidVacinaException extends RuntimeException {
+    public InvalidVacinaException(String message) {
+        super(message);
+    }
+}
