e1f64d4a78c1e558f378403c657496fce6f06f46
Otavio <estudosprogotavio@gmail.com>
2026-02-11 07:28:19 -0300

    refactor:movendo para um novo pacote CreateUsuarioInterface

--- src/main/java/dev/zerphyis/orangezup/domain/interfacecases/usuarios/CreateUsuarioInterface.java
+++ src/main/java/dev/zerphyis/orangezup/domain/interfacecases/usuarios/CreateUsuarioInterface.java
@@ -1,6 +1,6 @@
-package dev.zerphyis.orangezup.domain.interfacecases;
+package dev.zerphyis.orangezup.domain.interfacecases.usuarios;
 
-import dev.zerphyis.orangezup.application.dto.CreateUsuarioRequest;
+import dev.zerphyis.orangezup.application.dto.usuarios.CreateUsuarioRequest;
 import dev.zerphyis.orangezup.domain.entities.Usuario;
 
 public interface CreateUsuarioInterface {
