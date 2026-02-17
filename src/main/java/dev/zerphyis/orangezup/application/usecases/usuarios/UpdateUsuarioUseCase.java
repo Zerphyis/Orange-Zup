42c80eedf8f5a7b716721649bde796f7cc73274a
Otavio <estudosprogotavio@gmail.com>
2026-02-17 14:31:35 -0300

    refactor: alterando retorno para UsuarioResponse em UpdateUsuarioUseCase

--- src/main/java/dev/zerphyis/orangezup/application/usecases/usuarios/UpdateUsuarioUseCase.java
+++ src/main/java/dev/zerphyis/orangezup/application/usecases/usuarios/UpdateUsuarioUseCase.java
@@ -1,9 +1,11 @@
 package dev.zerphyis.orangezup.application.usecases.usuarios;
 
 import dev.zerphyis.orangezup.application.dto.usuarios.UpdateUsuarioRequest;
+import dev.zerphyis.orangezup.application.dto.usuarios.UsuarioResponse;
 import dev.zerphyis.orangezup.domain.entities.Usuario;
 import dev.zerphyis.orangezup.domain.entities.repository.UsuarioRepository;
 import dev.zerphyis.orangezup.domain.entities.vo.Email;
+import dev.zerphyis.orangezup.domain.exception.EmailAlreadyExistsException;
 import dev.zerphyis.orangezup.domain.exception.InvalidCpfException;
 import dev.zerphyis.orangezup.domain.interfacecases.usuarios.UpdateUsuarioInterface;
 
@@ -15,19 +17,32 @@ public class UpdateUsuarioUseCase implements UpdateUsuarioInterface {
         this.repository = repository;
     }
 
-
     @Override
-    public Usuario execute(String cpf, UpdateUsuarioRequest request) {
+    public UsuarioResponse execute(String cpf, UpdateUsuarioRequest request) {
+
         Usuario usuario = repository.findByCpf(cpf)
-                .orElseThrow(() -> new InvalidCpfException("Usuário não encontrado"));
+                .orElseThrow(() ->
+                        new InvalidCpfException("Usuário não encontrado"));
 
-        Usuario usuarioAtualizado = new Usuario(
-                request.nome(),
-                new Email(request.email()),
-                usuario.getCpf(),
-                request.dataNascimento()
+        if (!usuario.getEmail().value().equals(request.email())
+                && repository.existsByEmail(request.email())) {
+            throw new EmailAlreadyExistsException(request.email());
+        }
+
+        usuario.setNome(request.nome());
+        usuario.setEmail(new Email(request.email()));
+        usuario.setDataNascimento(request.dataNascimento());
+
+        Usuario updated = repository.save(usuario);
+
+        return new UsuarioResponse(
+                updated.getNome(),
+                updated.getEmail().value(),
+                updated.getCpf().value(),
+                updated.getDataNascimento()
         );
-
-        return repository.save(usuarioAtualizado);
     }
 }
+
+
+
