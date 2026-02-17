a0b0740bacd021c9bd5d662bde845f1680a0a376
Otavio <estudosprogotavio@gmail.com>
2026-02-17 14:30:14 -0300

    refactor: alterando retoro para UsuarioResponse em CreateUsuarioInterface

--- src/main/java/dev/zerphyis/orangezup/domain/interfacecases/usuarios/CreateUsuarioInterface.java
+++ src/main/java/dev/zerphyis/orangezup/domain/interfacecases/usuarios/CreateUsuarioInterface.java
@@ -1,8 +1,9 @@
 package dev.zerphyis.orangezup.domain.interfacecases.usuarios;
 
 import dev.zerphyis.orangezup.application.dto.usuarios.CreateUsuarioRequest;
+import dev.zerphyis.orangezup.application.dto.usuarios.UsuarioResponse;
 import dev.zerphyis.orangezup.domain.entities.Usuario;
 
 public interface CreateUsuarioInterface {
-    Usuario execute(CreateUsuarioRequest request);
+    UsuarioResponse execute(CreateUsuarioRequest request);
 }
