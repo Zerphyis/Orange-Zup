a0e5d45d2bbc46eea5321e1955b1425d5b13e9e2
Otavio <estudosprogotavio@gmail.com>
2026-02-17 14:31:14 -0300

    refactor: alterando retorno para UsuarioResponse em ListUsuarioUseCase

--- src/main/java/dev/zerphyis/orangezup/application/usecases/usuarios/ListUsuarioUseCase.java
+++ src/main/java/dev/zerphyis/orangezup/application/usecases/usuarios/ListUsuarioUseCase.java
@@ -1,19 +1,31 @@
 package dev.zerphyis.orangezup.application.usecases.usuarios;
 
+import dev.zerphyis.orangezup.application.dto.usuarios.UsuarioResponse;
 import dev.zerphyis.orangezup.domain.entities.Usuario;
 import dev.zerphyis.orangezup.domain.entities.repository.UsuarioRepository;
 import dev.zerphyis.orangezup.domain.interfacecases.usuarios.ListUsuarioInterface;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ListUsuarioUseCase  implements ListUsuarioInterface {
+public class ListUsuarioUseCase implements ListUsuarioInterface {
+
     private final UsuarioRepository repository;
 
     public ListUsuarioUseCase(UsuarioRepository repository) {
         this.repository = repository;
     }
-   @Override
-    public List<Usuario> execute() {
-        return repository.findAll();
+
+    @Override
+    public List<UsuarioResponse> execute() {
+        return repository.findAll()
+                .stream()
+                .map(usuario -> new UsuarioResponse(
+                        usuario.getNome(),
+                        usuario.getEmail().value(),
+                        usuario.getCpf().value(),
+                        usuario.getDataNascimento()
+                ))
+                .collect(Collectors.toList());
     }
 }
