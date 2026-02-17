78a7814a590cfa3cae0012ecece88efc47be3697
Otavio <estudosprogotavio@gmail.com>
2026-02-17 14:31:22 -0300

    refactor: alterando retorno para UsuarioResponse em UpdateUsuarioInterface

--- src/main/java/dev/zerphyis/orangezup/domain/interfacecases/usuarios/UpdateUsuarioInterface.java
+++ src/main/java/dev/zerphyis/orangezup/domain/interfacecases/usuarios/UpdateUsuarioInterface.java
@@ -1,8 +1,9 @@
 package dev.zerphyis.orangezup.domain.interfacecases.usuarios;
 
 import dev.zerphyis.orangezup.application.dto.usuarios.UpdateUsuarioRequest;
+import dev.zerphyis.orangezup.application.dto.usuarios.UsuarioResponse;
 import dev.zerphyis.orangezup.domain.entities.Usuario;
 
 public interface UpdateUsuarioInterface {
-    Usuario execute(String cpf, UpdateUsuarioRequest request);
+    UsuarioResponse execute(String cpf, UpdateUsuarioRequest request);
 }
